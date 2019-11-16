e96a6704b2fc787c0dcceb38147f10c55a6d32fe
coolbuch <kovdima2000@gmail.com>
2019-11-16 15:58:11 +0800

    AsteroidField

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Object[] res;
+    public List<Sprite> sprites = new List<Sprite>();
+    public float lifeTime = 10f;
+    Vector2 direction;
+    float timer = 0;
+    float spawnTimer = 0;
+    void Start()
+    {
+        direction = transform.up;
+        //speed = Random.Range(speed - 10, speed + 10);
+        rb = GetComponent<Rigidbody2D>();
+        ResetSprite();
+    }
+
+    void Update()
+    {
+        Move(direction);
+        timer += Time.deltaTime;
+    }
+
+    void Move(Vector2 direction)
+    {
+        rb.velocity = direction * speed * Time.deltaTime;
+        if (timer > lifeTime)
+        {
+            enabled = false;
+            timer = 0;
+        }
+    }
+
+    public void ResetSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[(int)Random.Range(0, sprites.Count)];
+        enabled = true;
+        if (rb != null)
+        {
+            rb.inertia = 0;
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    public void SetDirection(float angle)
+    {
+        this.direction = transform.up * Mathf.Tan(angle);
+        direction.Normalize();
+        transform.rotation = Quaternion.Euler(0, 0, angle * 180 / 3.14f);
+    }
+
+
+}
---
+++ Assets/Scripts/AsteroidField.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidField : MonoBehaviour
+{
+    List<Asteroid> asteroids = new List<Asteroid>();
+    [Header("Prefab")]
+    public Asteroid asteroid;
+    [Header("Width of line where asteroids will spawn")]
+    public float  width = 300;
+    Transform startPoint;
+    [Header("Asteroid count")]
+    public int asteriodCount = 10;
+    [Header("Distance beetwen asteroids")]
+    public float minOffset = 10 ,maxOffset = 15;
+    public float angle = 0;
+
+    int unactive = 0;
+    float timer = 0;
+    float spawnTimer = 0;
+    void Start()
+    {
+        startPoint = transform;
+        for (int i = 0; i < asteriodCount; i++)
+            asteroids.Add(Instantiate(asteroid, this.transform));
+        StartAsteroids();
+    }
+
+    void Update()
+    {
+        StartAsteroids();
+        spawnTimer += Time.deltaTime;
+    }
+
+    void StartAsteroids()
+    {
+        float nextY = startPoint.position.y;
+        float nextX = startPoint.position.x;
+        foreach (Asteroid a in asteroids)
+        {
+            if (spawnTimer > Random.Range(0.2f, 0.4f))
+            {
+                if (!a.enabled)
+                {
+                    Debug.Log(transform.position.y + transform.position.y * Mathf.Sin(angle));
+                    Spawn(a, new Vector2(startPoint.position.x + Random.Range(minOffset, maxOffset) * Mathf.Cos(angle), startPoint.position.y + Random.Range(minOffset, maxOffset) * Mathf.Sin(angle)));
+                    minOffset += 5;
+                    maxOffset += 5;
+                    if (maxOffset > width)
+                    {
+                        minOffset = 10;
+                        maxOffset = 20;
+                    }
+                }
+                spawnTimer = 0;
+            }
+        }
+    }
+
+    void Spawn(Asteroid a, Vector2 coords)
+    {
+        a.ResetSprite();
+        a.SetDirection(angle);
+        a.transform.position = coords;
+    }
+}
